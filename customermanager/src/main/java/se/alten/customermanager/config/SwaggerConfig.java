f1a98d55bdfc0fc917219d4d587574c66689a42c
ahmetwaly <ahmed.a.metwali@gmail.com>
2019-05-17 02:09:44 +0400

    updating docker file Image

--- customermanager/src/main/java/se/alten/customermanager/config/SwaggerConfig.java
+++ customermanager/src/main/java/se/alten/customermanager/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package se.alten.customermanager.Config;
+package se.alten.customermanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
