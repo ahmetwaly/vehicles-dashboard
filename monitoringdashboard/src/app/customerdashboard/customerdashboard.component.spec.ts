3b45e2b70a62773ec9a0bdcbf7f7e5b640cd76bb
ahmetwaly <ahmed.a.metwali@gmail.com>
2019-05-26 20:32:35 +0400

    updating docker file removing build stage

--- monitoringdashboard/src/app/customerdashboard/customerdashboard.component.spec.ts
+++ monitoringdashboard/src/app/customerdashboard/customerdashboard.component.spec.ts
@@ -1,29 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CustomerdashboardComponent } from './customerdashboard.component';
-import { Customer } from '../shared/dto/Customer';
+import { BrowserModule, By } from '@angular/platform-browser';
 import { CustomerVehiclesManagerService } from '../shared/services/customervehiclesmanagerservice.service';
-import {Observable, interval} from 'rxjs';
-import { environment } from '../../environments/environment';
+import { UserServiceMock } from '../../mocks/user.service.mock';
 
 
 describe('CustomerdashboardComponent', () => {
-  let component: CustomerdashboardComponent;
-  let fixture: ComponentFixture<CustomerdashboardComponent>;
+    let component: CustomerdashboardComponent;
+    let fixture: ComponentFixture<CustomerdashboardComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CustomerdashboardComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [CustomerdashboardComponent],
+            providers: [
+                { provide: CustomerVehiclesManagerService, useClass: UserServiceMock }
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CustomerdashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CustomerdashboardComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
