7fc843c6526e25c4c7716039fcb05952e2f2db5a
ahmetwaly <ahmed.a.metwali@gmail.com>
2019-05-26 23:55:32 +0400

    updating test cases

--- monitoringdashboard/src/app/customerdashboard/customerdashboard.component.spec.ts
+++ monitoringdashboard/src/app/customerdashboard/customerdashboard.component.spec.ts
@@ -47,12 +47,14 @@ describe('CustomerdashboardComponent', () => {
         expect(component.customers[0].vehicles.length).toEqual(3);
         component.ngOnDestroy();
     }));
-    // it('should render title in a h1 tag', async(() => {
-    //     const fixture = TestBed.createComponent(AppComponent);
-    //     fixture.detectChanges();
-    //     const compiled = fixture.debugElement.nativeElement;
-    //     expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular Unit Testing!');
-    // }));
+    it('should vehicle status filter', async(() => {
+        component.ngOnInit()
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        console.log(compiled.name)
+        expect(compiled.querySelectorAll('#customerNameDev').length).toEqual(3)
+        component.ngOnDestroy();
+    }));
 
     afterEach(() => {
         fixture.destroy();
